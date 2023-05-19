4e6e3b4e6814cd9ef0fcd2894f1a9314032be0f7
Ryther Anderson <randerson@treeline.bio>
2023-05-19 16:29:46 -0600

    Use Union instead of |

--- pyfracgen/types.py
+++ pyfracgen/types.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Union
 
 import numpy as np
 import numpy.typing as npt
 
-ResultArray = npt.NDArray[np.float32 | np.float64]
+ResultArray = npt.NDArray[Union[np.float32, np.float64]]
 Bound = Tuple[float, float]
 UpdateFunc = Callable[[complex, complex], complex]
