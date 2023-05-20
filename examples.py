04d1f14fa96295c5d2b3484f1ed61125acbc15a1
Ryther Anderson <rytheranderson@gmail.com>
2023-05-20 13:59:06 -0600

    Update examples

--- examples.py
+++ examples.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 import time
+from pathlib import Path
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -55,11 +56,11 @@ def mandelbrot_example() -> None:
         -0.6413130610648031748603750151793 - 0.00000000000006,
         -0.6413130610648031748603750151793 + 0.00000000000013,
     )
-    mymap = pf.images.stack_cmaps(plt.cm.gist_gray, 50)
-    man = pf.mandelbrot(
-        xbound, ybound, pf.funcs.power, width=4, height=3, maxiter=5000, dpi=300
+    res = pf.mandelbrot(
+        xbound, ybound, pf.funcs.power, width=4, height=3, dpi=300, maxiter=5000
     )
-    pf.images.image(man, cmap=mymap, gamma=0.8)
+    stacked = pf.images.get_stacked_cmap(plt.cm.gist_gray, 50)
+    pf.images.image(res, cmap=stacked, gamma=0.8)
     plt.savefig("example_images/mandelbrot_ex.png")
     print(f"calculation took {round((time.time() - start_time), 2)} seconds")
 
@@ -67,9 +68,9 @@ def mandelbrot_example() -> None:
 def julia_animation_example() -> None:
 
     start_time = time.time()
-    c_vals = [complex(i, 0.75) for i in np.linspace(0.05, 3.0, 100)]
-    s = pf.julia_series(
-        c_vals,
+    cvals = (complex(i, 0.75) for i in np.linspace(0.05, 3.0, 100))
+    series = pf.julia(
+        cvals,
         (-1, 1),
         (-0.75, 1.25),
         pf.funcs.magnetic_2,
@@ -79,10 +80,10 @@ def julia_animation_example() -> None:
         dpi=200,
     )
     pf.images.save_animation(
-        s,
-        gamma=0.9,
+        list(series),
         cmap=plt.cm.gist_ncar,
-        filename="example_images/julia_animation_ex",
+        gamma=0.9,
+        file=Path("example_images/julia_animation_ex"),
     )
     print(f"calculation took {round((time.time() - start_time), 2)} seconds")
 
@@ -93,10 +94,10 @@ def lyapunov_example() -> None:
     string = "AAAAAABBBBBB"
     xbound = (2.5, 3.4)
     ybound = (3.4, 4.0)
-    im = pf.lyapunov(
-        string, xbound, ybound, n_init=2000, n_iter=2000, dpi=300, width=4, height=3
+    res = pf.lyapunov(
+        string, xbound, ybound, width=4, height=3, dpi=300, ninit=2000, niter=2000
     )
-    pf.images.markus_lyapunov_image(im, plt.cm.bone, plt.cm.bone_r, gammas=(8, 1))
+    pf.images.markus_lyapunov_image(res, plt.cm.bone, plt.cm.bone_r, gammas=(8, 1))
     plt.savefig("example_images/lyapunov_ex.png")
     print(f"calculation took {round((time.time() - start_time), 2)} seconds")
 
@@ -106,10 +107,8 @@ def randomwalk_example() -> None:
     start_time = time.time()
     basis = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     moves = pf.construct_moves(basis)
-    M = pf.randomwalk(
-        moves, 5000000, width=4, height=3, depth=10, dpi=300, tracking="temporal"
-    )
-    pf.images.randomwalk_image(M, cmap=plt.cm.gist_yarg, gamma=1.0)
+    res = pf.randomwalk(moves, niter=5000000, width=4, height=3, depth=10, dpi=300)
+    pf.images.randomwalk_image(res, cmap=plt.cm.gist_yarg, gamma=1.0)
     plt.savefig("example_images/randomwalk_ex.png")
     print(f"calculation took {round((time.time() - start_time), 2)} seconds")
 
@@ -122,15 +121,15 @@ def buddhabrot_example() -> None:  # this will take awhile
     res = pf.buddhabrot(
         xbound,
         ybound,
-        10000000,
-        pf.funcs.power,
+        ncvals=10000000,
+        update_func=pf.funcs.power,
         horizon=1.0e6,
-        maxiters=[100, 1000, 10000],
-        width=5,
-        height=4,
+        maxiters=(100, 1000, 10000),
+        width=4,
+        height=3,
         dpi=300,
     )
-    pf.images.nebula_image(*list(res), gamma=0.4)  # type: ignore[arg-type, misc]
+    pf.images.nebula_image(tuple(res), gamma=0.4)  # type: ignore[arg-type]
     plt.savefig("example_images/buddhabrot_ex.png")
     print(f"calculation took {round((time.time() - start_time), 2)} seconds")
 
