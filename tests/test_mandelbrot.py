4e4d85b68deb9d9bd2966ff475a54a7762cc8ca5
Ryther Anderson <rytheranderson@gmail.com>
2023-12-17 06:46:45 -0700

    Revert "Ignore more test files"
    This reverts commit bb58d973bc9eacf52a64d3f3afab9c09a2b276c2.

--- tests/test_mandelbrot.py
+++ tests/test_mandelbrot.py
@@ -7,7 +7,6 @@ from pyfracgen.iterfuncs.funcs import power
 from pyfracgen.mandelbrot import Mandelbrot
 
 MAXITER = 1000
-HORIZON = 2.0**40
 
 
 def point_escapes(c: complex) -> bool:
@@ -23,7 +22,7 @@ def point_escapes(c: complex) -> bool:
 
 @given(c=st.complex_numbers(max_magnitude=4.0, allow_subnormal=False))
 def test_single_point_escape(c: complex) -> None:
-    """Test that a point (complex) escapes or does not escape as expected.
+    """Test that a single point escapes or does not escape as expected.
 
     TODO: Add escape criteria and citation.
 
@@ -31,24 +30,10 @@ def test_single_point_escape(c: complex) -> None:
         c: The single point to test.
     """
     mandelbrot = Mandelbrot(1, 1, 1, (c.real, c.real), (c.imag, c.imag))
-    mandelbrot.paint(power, MAXITER, HORIZON, False)
+    mandelbrot.paint(power, MAXITER, 2.0**40, False)
     color = mandelbrot.result.image_array[0, 0]
     if point_escapes(c):
         # No c^2 + c > the horizon => minimun 1 iteration required for escape
         assert 0.0 < color < MAXITER
     else:
         assert color == 0.0
-
-
-def test_points_outside_mandelbrot_are_colored() -> None:
-    """Test points outside the Mandelbrot set are colored."""
-    mandelbrot = Mandelbrot(1, 1, 100, (4.0, 5.0), (4.0, 5.0))
-    mandelbrot.paint(power, MAXITER, HORIZON, False)
-    assert mandelbrot.result.image_array.all()
-
-
-def test_points_inside_mandelbrot_are_not_colored() -> None:
-    """Test points in the Mandelbrot set are not colored."""
-    mandelbrot = Mandelbrot(1, 1, 100, (-0.2, 0.2), (-0.2, 0.2))
-    mandelbrot.paint(power, MAXITER, 2, False)
-    assert not mandelbrot.result.image_array.any()
